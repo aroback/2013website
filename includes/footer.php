af1a5c801a3c9a723728c0548f8e04701e418b1e
aroback <andrew.roback@gmail.com>
2017-05-24 23:34:33 -0500

    updated footer and associated styles

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
-<?php
-// footer.php
-// uniform footer text for all pages
-	echo "<p>ADD LICENSING INFORMATION AND LINK TO REPOSITORY</p>";
-?>
+	<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="../images/CC-BY-NC-SA-icon-80x15.png" /></a>
+	
+	<a rel="validator" href="http://validator.w3.org/check?uri=referer"><img alt="W3C Validator Icon" src="../images/valid_xhtml10_blue.png" /></a>
+	
+	<a href="http://jigsaw.w3.org/css-validator/check?uri=referer"><img alt="W3C CSS Validator Icon" src="../images/valid_css21_blue.png" /></a>
